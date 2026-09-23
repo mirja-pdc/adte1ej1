fae7661619da76813fce06b3d573f10f155edb36
Mirja <mperezdecalleja@ikaslea.birt.eus>
2026-09-23 13:46:14 +0200

    segundo commit con errores

--- TA1/src/main/java/AD01/TA1/App.java
+++ TA1/src/main/java/AD01/TA1/App.java
@@ -1,13 +1,31 @@
-package AD01.TA1;
+import java.io.File;
 
 /**
- * Hello world!
- *
+ * Prueba con vídeos de File de AD01 en mi Mac
+ * Uso File.separator para independencia entre ruta fichero y SO
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+    	System.out.println("Información sobre el fichero:");
+    	
+    	File f = new File(File.separator + "Users" 
+        	    + File.separator + "mirja" 
+        		+ File.separator + "Documentos" 
+        	    + File.separator + "DAM" 
+        		+ File.separator + "AD" 
+        	    + File.separator + "pruebaLectura.txt");
+    	
+    	if(f.exists()) {
+    		System.out.println("Nombre: " + f.getName());
+    		System.out.println("Ruta: " + f.getPath());
+    		System.out.println("Ruta absoluta: " + f.getAbsolutePath());
+    		System.out.println("Se puede leer: " + f.canRead());
+    		System.out.println("Se puede escribir: " + f.canWrite());
+    		System.out.println("Tamaño: " + f.length());
+    		System.out.println("Es un directorio: " + f.isDirectory());
+    		System.out.println("Es un fichero: " + f.isFile());
+    		System.out.println("Nombre del directorio padre: " + f.getParent());	
+    	}
+       
     }
 }
